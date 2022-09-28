164255e0a3a6fe878149100a631b1a0863c447ff
Lisnyi <russelhobbbs@gmail.com>
2022-09-28 15:54:41 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,13 @@
-import { nanoid } from "nanoid"
 import PropTypes from 'prop-types'
 import { OptionsList, OptionsItem } from "./FeedbackOptions.styled"
 
 export const FeedbackOptions = ({options, onLeaveFeedback}) => {
-    const voteButtons = options.map(option => (<OptionsItem key={nanoid()}>
-                                                    <button onClick={() => onLeaveFeedback(option)}>{option}</button>
-                                                </OptionsItem>))
 
     return  <OptionsList>
-               {voteButtons}
+               {options.map(option =>
+                    (<OptionsItem key={option}>
+                        <button onClick={() => onLeaveFeedback(option)}>{option}</button>
+                    </OptionsItem>))}
             </OptionsList>
 }
 
