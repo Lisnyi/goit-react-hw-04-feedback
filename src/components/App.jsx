9e0553ebfdea68abf6d7790dd3fb73df0dae283a
Lisnyi <russelhobbbs@gmail.com>
2022-10-07 13:22:42 +0300

    refactoring to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,53 @@
-import React, { Component } from 'react'
+import React, {  useReducer } from 'react'
 import { Section, FeedbackOptions, Statistics, Notification } from '../components'
 
-export default class App extends Component {
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+function optionsReducer (state, action) {
+  switch (action.type) {
+    case 'good':
+      return {...state, good: state.good + action.payload}
+    case 'neutral':
+      return {...state, neutral: state.neutral + action.payload}
+    case 'bad':
+      return {...state, bad: state.bad + action.payload}
+    default:
+      throw new Error(`Unsuported actin type ${action.type}`)
   }
+}
 
-  onLeaveFeedback = (option) => {
-    this.setState(prevState => {
-      return {[option]:prevState[option]+1} 
-    })
-  }
-
-  countTotalFeedback (...args) {
+export const App = () => {
+  const [state, dispatch] = useReducer(optionsReducer, initialState);
+  const { good, neutral, bad } = state
+  const options = Object.keys(state)
+  const total = countTotalFeedback(good, neutral, bad)
+  const positivePercentage = countPositiveFeedbackPercentage(good, total)
+  
+  function countTotalFeedback (...args) {
     const total = args.reduce((sum, current) => sum + current, 0)
     return total
   }
-  
-  countPositiveFeedbackPercentage (good, total) {
+
+  function countPositiveFeedbackPercentage (good, total) {
     const positivePercentage = good/total*100
     return Number(positivePercentage.toFixed(0))
   }
 
-  render() {
-    const { good, neutral, bad } = this.state
-    const total = this.countTotalFeedback(good, neutral, bad)
-    const positivePercentage = this.countPositiveFeedbackPercentage(good, total)
-    const options = Object.keys(this.state)
-    return (
-      <>
-        <Section title={"Please leave feedback"}>
-          <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        <Section title={"Statistics"}>
-          {total
-          ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/>
-          : <Notification message={"There is no feedback"}/>}         
-        </Section>
-      </>
-    )
-  }
+  return (
+    <>
+      <Section title={"Please leave feedback"}>
+        <FeedbackOptions options={options} leaveFeedback={dispatch}/>
+      </Section>
+      <Section title={"Statistics"}>
+        {total
+        ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/>
+        : <Notification message={"There is no feedback"}/>}         
+      </Section>
+    </>
+  )
 }
+
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types'
 import { OptionsList, OptionsItem } from "./FeedbackOptions.styled"
 
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+export const FeedbackOptions = ({options, leaveFeedback}) => {
 
     return  <OptionsList>
                {options.map(option =>
                     (<OptionsItem key={option}>
-                        <button onClick={() => onLeaveFeedback(option)}>{option}</button>
+                        <button onClick={() => leaveFeedback({type: `${option}`, payload: 1})}>{option}</button>
                     </OptionsItem>))}
             </OptionsList>
 }
 
 FeedbackOptions.propTypes = {
     options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired
+    leaveFeedback: PropTypes.func.isRequired
 }
